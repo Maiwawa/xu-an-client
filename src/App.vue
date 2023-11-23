ef5a7be1895087ab48fcda56181ab907309bff5e
Kamaboko0126 <maiwa0126@gmail.com>
2023-11-23 09:30:03 +0800

    first setting

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <ConnectApi/>
+</template>
+
+<script>
+import ConnectApi from './components/ConnectApi.vue';
+
+export default {
+  components: {
+    ConnectApi
+}
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/ConnectApi.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>{{ answer }}</div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "ConnectAPI",
+  data() {
+    return {
+      data:{},
+    };
+  },
+  mounted(){
+    this.connectCheck();
+  },
+  methods: {
+    async connectCheck() {
+      const { data } = await axios.get("http://127.0.0.1:8000/ConnectCheck");
+      this.answer = data;
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
